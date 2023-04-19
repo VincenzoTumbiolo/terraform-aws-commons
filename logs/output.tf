d74271732f305bfa050d43dde8f7601b61d8558f
VincenzoTumbiolo <66414892+VincenzoTumbiolo@users.noreply.github.com>
2023-04-19 17:37:33 +0200

    added security group for bastionHost

--- logs/output.tf
+++ logs/output.tf
@@ -8,6 +8,10 @@ output "rds_security_group" {
   value = aws_security_group.rds
 }
 
+output "bastion_host_security_group" {
+  value = aws_security_group.bastion_host
+}
+
 output "rds_cluster" {
   value = aws_rds_cluster.this
 }
--- logs/security_groups.tf
+++ logs/security_groups.tf
@@ -3,7 +3,23 @@ data "aws_vpc" "this" {
   id = var.vpcId
 }
 
-# aws security group for OtoPad CAS DB
+# aws security group for BastionHost
+
+resource "aws_security_group" "bastion_host" {
+  name   = "${var.env}-${local.tags.project_prefix}-sg-bastion-host-rds"
+  vpc_id = aws_vpc.this.id
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(local.tags, { Name = "${var.env}-${local.tags.project_prefix}-sg-bastion-host-rds" })
+}
+
+# aws security group for DB
 resource "aws_security_group" "rds" {
   name   = "${var.env}-${local.tags.project_prefix}-sg-rds"
   vpc_id = var.vpcId
